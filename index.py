6dfe5c05b1d03f8b603ea5bafd3990f9e570f271
abraaom <abraaol.moreira@outlook.com>
2021-08-17 01:11:17 -0300

    [feat] Intervalo de obtenção de dados
    Possibilita obter um intervalo de dados do banco de dados baseado em uma data inicial e uma data final.

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
-import pandas as pd
+from datetime import datetime
 
-dataframe = pd.read_csv('bitstampUSD_1-min_data_2012-01-01_to_2021-03-31.csv')
+import pandas
 
-print(dataframe)
+dataframe = pandas.read_csv('bitstampUSD_1-min_data_2012-01-01_to_2021-03-31.csv')
+
+startDate = "01/01/2020"#input("Data de início (DD/MM/AAAA): ")
+finishDate = "01/02/2020"#input("Data de fim (DD/MM/AAAA): ")
+
+startDateTimestamp = datetime.timestamp(datetime.strptime(startDate, "%d/%m/%Y"))
+finishDateTimestamp = datetime.timestamp(datetime.strptime(finishDate, "%d/%m/%Y"))
+
+interval = dataframe[dataframe['Timestamp'] >= startDateTimestamp]
+
+interval = interval[interval['Timestamp'] <= finishDateTimestamp]
+
+print(interval)
